21351fa53322a6ad1ff7ea0ffc80c4518f334c01
jontumalle <34110518+jontumalle@users.noreply.github.com>
2026-09-13 13:41:23 -0400

    Remove todo comment for lab2 part D

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -64,7 +64,7 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    # Lab 2 Part D
     curr_time = time.strftime("%m/%d/%Y %H:%M:%S")
     draw.text((x, top), curr_time, font=font, fill="#FFFFFF")
 
